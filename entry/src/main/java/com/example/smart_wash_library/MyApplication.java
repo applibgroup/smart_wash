e4d6a09121428d43a47c4abf4194d20dfcb908fa
HrideshSingh <hrideshsingh619@gmail.com>
2022-03-10 10:28:03 +0530

    First Commit

---
+++ entry/src/main/java/com/example/smart_wash_library/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.smart_wash_library;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
